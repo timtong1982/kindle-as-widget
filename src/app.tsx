dc9b101b670ce0fe3f6bb359ec7379e6e8202b2f
timtong1982 <mingtong@outlook.com>
2021-02-08 23:54:27 -0800

    save

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import * as React from 'React';
+import * as React from 'react';
 import { AppContext, AppContextProvider } from './appContext';
 import { appReducer, initialAppState } from './appReducer';
 import { Time } from './time';
--- src/appContext.tsx
+++ src/appContext.tsx
@@ -1,4 +1,4 @@
-import * as React from 'React';
+import * as React from 'react';
 import { createContext } from 'use-context-selector';
 import { appReducer, initialAppState } from './appReducer';
 
